bf2dc91425a697618c567cfd506a9ceb0e2cc5ec
SieR-VR <nwh040512@gmail.com>
2023-03-25 23:13:43 +0900

    v1.2.0 change

--- scripts/result.lua
+++ scripts/result.lua
@@ -32,11 +32,11 @@ local badgeImages = {
 local laneNames = {"A", "B", "C", "D", "L", "R"}
 local diffNames = {"NOV", "ADV", "EXH", "INF"}
 local backgroundImage = gfx.CreateSkinImage("bg.png", 1);
-game.LoadSkinSample("applause")
+game.LoadSkinSample("applause.wav")
 local played = false
 local shotTimer = 0;
 local shotPath = "";
-game.LoadSkinSample("shutter")
+game.LoadSkinSample("shutter.wav")
 local highScores = nil
 
 local highestScore = 0
